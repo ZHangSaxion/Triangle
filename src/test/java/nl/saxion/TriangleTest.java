087302d2a7a479c487251ef26a4bdf3ed2debcd9
KarliH <54204989+KarliH@users.noreply.github.com>
2020-09-22 19:28:19 +0200

    add commons

--- src/test/java/nl/saxion/TriangleTest.java
+++ src/test/java/nl/saxion/TriangleTest.java
@@ -4,21 +4,43 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * A test to detect whether the Triangle class has correct functions.
+ * @author Ziru Hang 486545
+ */
 public class TriangleTest {
+
+    /**
+     *  To test whether the class can recognize the input is not a triangle.
+     */
     @Test
     public void testNotTriangle(){
         Triangle notATriangle = new Triangle(1,2,4);
         assertEquals("not a triangle", notATriangle.getTypeBySides());
     }
+
+    /**
+     *  To test whether the class can recognize a equilateral triangle.
+     */
     @Test
     public void testEquilateral(){
         Triangle equilateral = new Triangle(1,1,1);
         assertEquals("Equilateral", equilateral.getTypeBySides());
-    }    @Test
+    }
+
+    /**
+     *  To test whether the class can recognize a isosceles triangle.
+     */
+    @Test
     public void testIsosceles(){
         Triangle isosceles = new Triangle(1, 2, 2);
         assertEquals("Isosceles", isosceles.getTypeBySides());
-    }    @Test
+    }
+
+    /**
+     *  To test whether the class can recognize a scalene triangle
+     */
+    @Test
     public void testScalene(){
         Triangle scalene = new Triangle(2,3,4);
         assertEquals("Scalene", scalene.getTypeBySides());
